63c27c4208ba86a9a0ea358020062b9a1413a16c
raheeldouglas <raheeldouglas@yahoo.com>
2022-11-12 00:37:30 -0500

    updated

--- edit.php
+++ edit.php
@@ -1,7 +1,8 @@
 <?php 
     $title = 'Edit Record';
 
-    require_once 'includes/header.php'; 
+    require_once 'includes/header.php';
+    require_once 'includes/auth_check.php';
     require_once 'db/conn.php';
 
     $results = $crud->getSpecialties();
--- login.php
+++ login.php
@@ -3,7 +3,56 @@
 
     require_once 'includes/header.php'; 
     require_once 'db/conn.php';
-    
+    // require_once 'db/user.php';
+
+    // If data was submitted via a from POST request, then...
+    if($_SERVER['REQUEST_METHOD'] == 'POST'){
+        $username = strtolower(trim($_POST['username']));
+        $password = $_POST['password'];
+        $new_password = md5($password.$username);
+        
+        $result = $user->getUser($username,$new_password);
+
+        if(!$result){
+            echo '<div class="alert alert-danger">Username or Password is incorrect! Please Enter Correct Password. </div>';
+        }else{
+            $_SESSION['username'] = $username;
+            $_SESSION['user_id'] = $result['id'];
+            var_dump($_SESSION);
+            header("Location: viewrecords.php");
+        }
+    }
 ?>
 
-<h1 class = "text-center"
+<h1 class = "text-center"><?php echo $title ?> </h1>
+   
+    <form action="<?php echo htmlentities($_SERVER['PHP_SELF']); ?>" method="post">
+        <table class="table table-sm">
+            <tr>
+                <td>
+                    <label for="username">Username: * </label></td>
+                <td><input type="text" name="username" class="form-control" id="username" 
+                value="<?php if($_SERVER['REQUEST_METHOD'] == 'POST') echo $_POST['username']; ?>">
+                
+                <?php if (empty($username) && $_SERVER['REQUEST_METHOD'] == 'POST') echo "<p class='text-danger'>$username</p>"; ?>
+                </td>
+            </tr>
+            <tr>
+                <td><label for="password">Password: * </label></td>
+                <td><input type="password" name="password" class="form-control" id="password">
+                <?php if (empty($password) && isset($password_error)) echo "<p class='text-danger'>$password_error</p>"; ?>
+                </td>
+            </tr>
+        </table><br/><br/><br/>
+        <div class="d-grid gap-2">
+        <input type="submit" value="Login" class="btn btn-primary btn-block"><br/>
+
+            <!-- <button type="submit" name="Login" class="btn btn-primary btn-block">Login</button>    -->
+        </div>
+
+        <br/><br/><br/>
+        <a href="#"> Forget Password </a>
+
+    </form><br/><br/><br/>
+
+<?php include_once 'includes/footer.php'?>    
